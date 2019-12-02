d265bd8f05e2bc8dad71a1bce0f32132b05bc3e5
daohoangtuyenkc <56294182+daohoangtuyenkc@users.noreply.github.com>
2019-12-02 22:20:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,47 +5,39 @@
 	 <title>PHP</title>
 </head>
 <body>
-     <?php
-     $i = 0;
-	   $i =++;
-	   echo '<table border = "1" cellspacing = "0" cellpadding = "0" width = "700px" >';
-	   for($i=0; $i<=100; $i++){
-		 echo "<tr>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "<td><center>$i</center></td>";
-		 echo "</tr>";
-		 
-	   }
-	   echo '</table>';
-	 
-	   ?>
-	 <fieldset style="width: 500px; margin: 0 auto">
-	     <legend><strong>FORM</strong></legend>
-		   <form action="index.php" method="post">
+     <table border="1" width="700px" height="700px" cellspacing="0">
+     <?php 
+	     $a =0;
+		 for($i=1;$i<=10; $i++)
+		 { echo "<tr>";
+	       for ($j=1;$j<=10;$j++)
+		   {
+			   echo "<td><center>".$a."</td></center>";
+			   $a++;
+			   
+		   }
+			echo "</tr>";
+		 }
+      ?>
+    </table>     
+     <fieldset style="width: 500px; margin: 0 auto">
+	      <legend><strong>FORM</strong></legend>
+		  <form  method="post">
 		   <label>Большая днина прямоугольного параллелепипеда</label><br>
-		   <input type="float" name="Большая днина" placeholder="Большая днина"><br>
+		   <input type="number" name="Большая_днина" placeholder="Большая днина" value="1"><br>
 		   <label>Меньшая днина прямоугольного параллелепипеда</label><br>
-		   <input type="float" name="Меньшая днина" placeholder="Меньшая днина"><br>
+		   <input type="number" name="Меньшая_днина" placeholder="Меньшая днина" value="2"><br>
 		   <label>Высота прямоугольного параллелепипеда</label><br>
-		   <input type="float" name="Высота" placeholder="Высота"><br> <br>
+		   <input type="number" name="Высота" placeholder="Высота" value="3"><br> <br>
 		   <input type="submit" name="submit" value="Выводит результат"><br>
-		   </form>
-		 
+		  </form>
 	 </fieldset>
 	 <?php
-	   $S1 = $_POST['Меньшая днина']*$_POST['Большая днина'];
-		 $S2 = $_POST['Меньшая днина']*$_POST['Высота'];
-		 $S3 = $_POST['Высота']*$_POST['Большая днина'];
-		 $V = $_POST['Меньшая днина']*$_POST['Большая днина']*$_POST['Высота'];
 	     if(isset($_POST['submit'])){
+			 $S1 = $_POST['Меньшая_днина']*$_POST['Большая_днина'];
+		         $S2 = $_POST['Меньшая_днина']*$_POST['Высота'];
+		         $S3 = $_POST['Высота']*$_POST['Большая_днина'];
+		         $V = $_POST['Меньшая_днина']*$_POST['Большая_днина']*$_POST['Высота'];
 			 echo "Площадь параллелепипеда 1 = ".$S1;
 			 echo "<hr>";
 			 echo "Площадь параллелепипеда 2 = ".$S2;
@@ -58,6 +50,6 @@
              echo "Пока нет габариты";
              echo "<hr>";			 
 		 }
-	 ?>
+	 ?>	 
 </body>
 </html>
